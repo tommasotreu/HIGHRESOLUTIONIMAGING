13469a68c536a0dd027e659621df3322d1fdcf05
Xiaolei Meng <xlmeng@scorza2.physics.ucsb.edu>
2014-08-21 16:21:42 -0700

    minor corrections

--- codes4inspection/perform_HST_50s.py
+++ codes4inspection/perform_HST_50s.py
@@ -2,9 +2,9 @@ import lens_fitting
 import numpy
 
 workdir=    '/home/xlmeng/python_scorza2/GitHub_push/'
-sys_name=   workdir+'/HST/src_lens_HST_para.txt'
-instr_name= workdir+'/HST/tele_para.txt'
-ep_name=    workdir+'/HST/ep_para.txt'
+sys_name=   workdir+'HST/src_lens_HST_para.txt'
+instr_name= workdir+'HST/tele_para.txt'
+ep_name=    workdir+'HST/ep_para.txt'
 psf_name=   workdir+'PSF/HST_F814W.fits'
 
 sys_allpara=numpy.genfromtxt(sys_name,dtype=float,comments='#')
@@ -20,4 +20,4 @@ ep_50s=ep_allpara[0,:]
 #lens_fitting.run(sys_1, instr_HST, ep_50s, psf_name, 'temp_sample.dat1', Nsample=50) 
 
 # run the whole file, i.e., 2 lines  <=>  2 lens systems
-lens_fitting.run(sys_allpara, instr_HST, ep_50s, psf_name, 'HST50s.pikl', Nsample=2000) 
+lens_fitting.run(sys_allpara, instr_HST, ep_50s, psf_name, 'HST50s.cpikl', Nsample=2000)
